fff2e82aa2ea4ea659761c50b7d7766d6b0fefeb
Zachary Cloutier <zcloutie@uci.edu>
2018-04-30 01:15:34 -0700

    Imported Ship, Added Simple Movement, Added Basic Networking

---
+++ Unity Project/ICS168Project1/Assets/Scripts/PlayerControllerScript.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using UnityEngine.Networking;
+
+public class PlayerControllerScript : NetworkBehaviour
+{
+	public float speed = 3.0F;
+	public float rotateSpeed = 3.0F;
+	public Camera cam;
+
+
+	void Update()
+	{
+		if (!isLocalPlayer)
+		{
+			return;
+		}
+			
+		CharacterController controller = GetComponent<CharacterController>();
+		transform.Rotate(0, Input.GetAxis("Horizontal") * rotateSpeed, 0);
+		Vector3 forward = transform.TransformDirection(Vector3.forward);
+		float curSpeed = speed * Input.GetAxis("Vertical");
+		controller.SimpleMove(forward * curSpeed);
+	}
+
+	public override void OnStartLocalPlayer()
+	{
+		GetComponent<Renderer>().material.color = Color.blue;
+	}
+}
