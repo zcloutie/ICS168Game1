c843ce1234487dac4196c0771a918667dad94faf
Zachary Cloutier <zcloutie@uci.edu>
2018-04-30 20:02:43 -0700

    Added Ship Driving

--- Unity Project/ICS168Project1/Assets/Scripts/PlayerControllerScript.cs
+++ Unity Project/ICS168Project1/Assets/Scripts/PlayerControllerScript.cs
@@ -7,23 +7,78 @@ public class PlayerControllerScript : NetworkBehaviour
 	public float rotateSpeed = 3.0F;
 	public Camera cam;
 
+	private bool isdriving = false;
+	private bool intrigger = false;
+	public GameObject ship;
+	private ShipController shipscript;
+	private Camera shipcam;
+
+	void Start(){
+		ship = GameObject.FindGameObjectWithTag ("Ship");
+		shipscript = ship.GetComponent<ShipController>();
+		shipcam = ship.GetComponentInChildren<Camera> ();
+		this.transform.parent = ship.transform;
+		if (!isLocalPlayer)
+		{
+			cam.enabled = false;
+		}
+	}
 
 	void Update()
 	{
 		if (!isLocalPlayer)
 		{
+			cam.enabled = false;
 			return;
 		}
-			
-		CharacterController controller = GetComponent<CharacterController>();
-		transform.Rotate(0, Input.GetAxis("Horizontal") * rotateSpeed, 0);
-		Vector3 forward = transform.TransformDirection(Vector3.forward);
-		float curSpeed = speed * Input.GetAxis("Vertical");
-		controller.SimpleMove(forward * curSpeed);
+
+		if (!isdriving) {
+			cam.enabled = true;
+		}
+		if (intrigger) {
+			if (Input.GetKeyUp ("e")) {
+				if (!shipscript.driven) {
+					isdriving = true;
+					shipscript.driven = true;
+					cam.enabled = false;
+					shipcam.enabled = true;
+
+				} else if (isdriving) {
+					isdriving = false;
+					shipscript.driven = false;
+					cam.enabled = true;
+					shipcam.enabled = false;
+				}
+			}
+		}
+		if (!isdriving) {
+			CharacterController controller = GetComponent<CharacterController> ();
+			transform.Rotate (0, Input.GetAxis ("Horizontal") * rotateSpeed, 0);
+			Vector3 forward = transform.TransformDirection (Vector3.forward);
+			float curSpeed = speed * Input.GetAxis ("Vertical");
+			controller.SimpleMove (forward * curSpeed);
+		}
+		else{
+			shipscript.Drive ();
+		}
 	}
 
 	public override void OnStartLocalPlayer()
 	{
 		GetComponent<Renderer>().material.color = Color.blue;
+		cam.enabled = true;
+	}
+
+	void OnTriggerEnter(Collider other){
+		if (other.tag == "Ship") {
+			intrigger = true;
+			Debug.Log ("OK");
+		}
+	}
+
+	void OnTriggerExit(Collider other){
+		if (other.tag == "Ship") {
+			intrigger = false;
+		}
 	}
 }
